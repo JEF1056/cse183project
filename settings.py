fd1fb36f2c9e789ac5a4fdb662bb894c3623cea7
StanleyHuang <xufanhuang@Stanleys-MacBook-M1-2.local>
2022-05-13 18:04:34 -0700

    test

--- settings.py
+++ settings.py
@@ -17,10 +17,11 @@ APP_NAME = os.path.split(APP_FOLDER)[-1]
 # DB_FOLDER:    Sets the place where migration files will be created
 #               and is the store location for SQLite databases
 DB_FOLDER = required_folder(APP_FOLDER, "databases")
-DB_URI = "mysql://root:"+password+"@sv.jfan.ml:34563/test?set_encoding =utf8mb4"
+DB_URI = "sqlite://storage.db"
+    #"mysql://root:"+password+"@sv.jfan.ml:34563/test?set_encoding =utf8mb4"
     #"sqlite://storage.db"
 DB_POOL_SIZE = 1
-DB_MIGRATE = False
+DB_MIGRATE = True
 DB_FAKE_MIGRATE = False  # maybe?
 
 # location where static files are stored:
@@ -29,10 +30,7 @@ STATIC_FOLDER = required_folder(APP_FOLDER, "static")
 # location where to store uploaded files:
 UPLOAD_FOLDER = required_folder(APP_FOLDER, "uploads")
 
-
-
-# send email on regstration
-
+# send email on registration
 VERIFY_EMAIL = True
 
 # account requires to be approved ?
